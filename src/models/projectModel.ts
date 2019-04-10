6d0d5eb514cff0f1151d472af49a8213648a6429
Simon Norman <01secn@gmail.com>
2019-04-10 22:02:37 +0100

    Add project with name completed

--- src/models/projectModel.ts
+++ src/models/projectModel.ts
@@ -3,13 +3,13 @@ import mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 interface Project {
-
+  name: string;
 }
 
 const ProjectSchema = new Schema({
   name: { type: String, required: true },
 });
 
-const Project = mongoose.model<mongoose.Document>('project', ProjectSchema);
+const Project = mongoose.model<Project & mongoose.Document>('project', ProjectSchema);
 
 export default Project;
